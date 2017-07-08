8c46e6547160003d87bef469f948737f1d90f6c9
Ashok Kumar Goli <ashok2ashok@gmail.com>
2017-07-08 16:57:19 -0500

    URL & Manifest updates

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,19 @@
 console.log('background');
+
+function onWebNav(details) {
+    if (details.frameId === 0) {
+        // Top-level frame
+        chrome.pageAction.show(details.tabId);
+    }
+}
+var filter = {
+    url: [{
+        hostEquals: 'trello.com'
+    }]
+};
+chrome.webNavigation.onCommitted.addListener(onWebNav, filter);
+chrome.webNavigation.onHistoryStateUpdated.addListener(onWebNav, filter);
+
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     chrome.tabs.executeScript(null,{file:"content.js"});
 });
